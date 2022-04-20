4a9bb798f7eba5a48e15ee6e1f5a3f6cf87c83ea
Viet Vu <jooservices@gmail.com>
2022-04-20 17:00:38 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y php-dev
 sudo apt -y install php-cli php-mbstring php-pecl-http \
-  php8.1-cli php8.1-curl php8.1-dev php8.1-http php8.1-igbinary php8.1-intl php8.1-mbstring php8.1-mcrypt php8.1-xml php8.1-xmlrpc php8.1-xsl php8.1-yaml php8.1-zip
+  php8.1-cli php8.1-curl php8.1-dev php8.1-igbinary php8.1-intl php8.1-mbstring php8.1-mcrypt php8.1-xml php8.1-xmlrpc php8.1-xsl php8.1-yaml php8.1-zip
 sudo apt -y install php-imagick php-gd php8.1-gd php8.1-imagick imagemagick
 sudo apt -y install php8.1-opcache php8.1-memcache php8.1-memcached php8.1-apcu
 sudo apt -y install php8.1-mongodb php8.1-mysql php8.1-redis php8.1-sqlite3
