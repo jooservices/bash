fe795651dd6a559d4f89aee65c075f73419ed60b
Viet Vu <jooservices@gmail.com>
2022-04-27 09:37:12 +0700

    Update rspi-php8.0.sh

--- rspi-php8.0.sh
+++ rspi-php8.0.sh
@@ -15,7 +15,7 @@ sudo usermod -aG docker ${USER}
 newgrp docker
 
 # PHP
-sudo apt install software-properties-common
+sudo apt -y install software-properties-common
 sudo apt-get -y install apt-transport-https lsb-release ca-certificates curl
 sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 sudo sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
