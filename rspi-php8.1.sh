8a442a99ef22f08b133d5e5a1e44206d8290e4e1
Viet Vu <jooservices@gmail.com>
2022-04-27 11:19:05 +0700

    Update and rename pi-php8.1.sh to rspi-php8.1.sh

--- rspi-php8.1.sh
+++ rspi-php8.1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt -y install git 
+sudo apt -y install git
+
 cd ~
 # Docker
 sudo apt-get install \
@@ -15,26 +16,34 @@ sudo usermod -aG docker ${USER}
 newgrp docker
 
 # PHP
-sudo apt install software-properties-common
-sudo apt-get -y install apt-transport-https lsb-release ca-certificates curl
+sudo apt -y install software-properties-common
+sudo apt -y install apt-transport-https lsb-release ca-certificates curl
 sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 sudo sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
 
 sudo apt update && sudo apt upgrade -y
 
+sudo apt install -y php8.1-dev
 sudo apt install -y php-dev
-sudo apt -y install php-cli php-mbstring \
-  php8.1-cli php8.1-curl php8.1-dev php8.1-igbinary php8.1-intl php8.1-mbstring php8.1-mcrypt php8.1-xml php8.1-xmlrpc php8.1-xsl php8.1-yaml php8.1-zip
-sudo apt -y install php-imagick php-gd php8.1-gd php8.1-imagick imagemagick
+sudo apt -y install php8.1-cli php8.1-curl php8.1-igbinary php8.1-intl php8.1-mbstring php8.1-mcrypt php8.1-xml php8.1-xmlrpc php8.1-xsl php8.1-yaml php8.1-zip
+sudo apt -y install php8.1-gd php8.1-imagick imagemagick
 sudo apt -y install php8.1-opcache php8.1-memcache php8.1-memcached php8.1-apcu
 sudo apt -y install php8.1-mysql php8.1-sqlite3
+
+# Set PHP 8.1
+sudo update-alternatives --set php /usr/bin/php8.1
+sudo update-alternatives --set php-config /usr/bin/php-config8.1
+sudo update-alternatives --set phpize /usr/bin/phpize8.1
 
 # Composer
 wget -O composer-setup.php https://getcomposer.org/installer
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 sudo apt -y autoclean && sudo apt -y autoremove
+
+# SSH
+ssh-keygen -t rsa -b 4096 -C "jooservices@gmail.com" -f ~/.ssh/id_rsa -N ''
 
 echo ~
 mkdir runner1
