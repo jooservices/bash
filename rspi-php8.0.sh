0d508193313b6751725a4de819bbb001319080fc
Viet Vu <jooservices@gmail.com>
2022-04-27 09:52:37 +0700

    Update rspi-php8.0.sh

--- rspi-php8.0.sh
+++ rspi-php8.0.sh
@@ -46,7 +46,9 @@ sudo apt -y autoclean && sudo apt -y autoremove
 # SSH
 ssh-keygen -t rsa -b 4096 -C "jooservices@gmail.com" -f ~/.ssh/id_rsa -N ''
 
-echo ~
+# Github
+
+cd ~
 mkdir runner1
 cd runner1
 mkdir actions-runner && cd actions-runner
