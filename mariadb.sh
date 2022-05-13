24700381fc6c199ccf8eaf9ae3a01cf59efd6961
Viet Vu <jooservices@gmail.com>
2022-05-13 10:12:32 +0700

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install curl apt-transport-https software-properties-common lsb-release ca-certificates gnupg2
+sudo apt install -y curl apt-transport-https software-properties-common lsb-release ca-certificates gnupg2
 curl -LsS -O https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
 sudo bash mariadb_repo_setup --mariadb-server-version=10.7
