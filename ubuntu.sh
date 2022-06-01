bf4e6798d9ed5fbf9f4b530010d8c6c3f332f532
Viet Vu <jooservices@gmail.com>
2022-06-01 16:28:42 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -45,9 +45,7 @@ sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 sudo apt -y autoclean && sudo apt -y autoremove
 
 echo ~
-mkdir runner
-cd runner
 mkdir actions-runner && cd actions-runner
-curl -o actions-runner-linux-x64-2.290.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.290.1/actions-runner-linux-x64-2.290.1.tar.gz
-echo "2b97bd3f4639a5df6223d7ce728a611a4cbddea9622c1837967c83c86ebb2baa  actions-runner-linux-x64-2.290.1.tar.gz" | shasum -a 256 -c
-tar xzf ./actions-runner-linux-x64-2.290.1.tar.gz
+curl -o actions-runner-osx-x64-2.291.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.291.1/actions-runner-osx-x64-2.291.1.tar.gz
+echo "1ed51d6f35af946e97bb1e10f1272197ded20dd55186ae463563cd2f58f476dc  actions-runner-osx-x64-2.291.1.tar.gz" | shasum -a 256 -c
+tar xzf ./actions-runner-osx-x64-2.291.1.tar.gz
