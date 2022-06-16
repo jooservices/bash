6f41b45fa94e24ddd2e705c96fe25e63e687849c
Viet Vu <jooservices@gmail.com>
2022-06-16 17:25:40 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -7,10 +7,10 @@ sudo apt-get install \
     gnupg \
     lsb-release
     
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo usermod -aG docker ${USER}
 
