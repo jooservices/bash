efb4483f49c005f3369ec2c3a29d83347ab55138
Viet Vu <jooservices@gmail.com>
2022-06-16 15:24:22 +0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+email="jooservices@gmail.com"
+name="Viet Vu"
+
 sudo apt update && sudo apt upgrade -y
 sudo apt -y install git
-git config --global user.email "jooservices@gmail.com"
-git config --global user.name "Viet Vu"
+git config --global user.email ${email}
+git config --global user.name ${name}
+
+$ ssh-keygen -t ed25519 -C $email
