d07c39e9e16902f44b6b6a54b75ae8dbf693b853
Viet Vu <jooservices@gmail.com>
2022-06-16 17:10:38 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -7,13 +7,12 @@ sudo apt-get install \
     gnupg \
     lsb-release
     
-curl -sSL https://get.docker.com | sh
-sudo groupadd docker
-sudo usermod -aG docker ${USER}
-newgrp docker
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo systemctl status docker
-
-docker run hello-world
