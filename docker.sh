bd6b7d4edb3220e9176d709fb7c2c035dba9c042
Viet Vu <jooservices@gmail.com>
2022-06-16 17:05:44 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -8,9 +8,12 @@ sudo apt-get install \
     lsb-release
     
 curl -sSL https://get.docker.com | sh
+sudo groupadd docker
 sudo usermod -aG docker ${USER}
 newgrp docker
 
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker
+
+docker run hello-world
