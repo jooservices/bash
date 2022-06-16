3dcf1f52bc38cd541f7ea8d9daa6e101a5ae3437
Viet Vu <jooservices@gmail.com>
2022-06-16 17:50:48 +0700

    Update and rename mariadb.sh to services/mariadb.sh

--- services/mariadb.sh
+++ services/mariadb.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
+version='10.8'
+
 sudo apt install -y curl apt-transport-https software-properties-common lsb-release ca-certificates gnupg2
 curl -LsS -O https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
-sudo bash mariadb_repo_setup --mariadb-server-version=10.7
+sudo bash mariadb_repo_setup --mariadb-server-version=${version}
+
 sudo apt update && sudo apt upgrade -y
 apt policy mariadb-server
 sudo apt install -y mariadb-server mariadb-client
