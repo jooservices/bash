9f2e24d3c5b65db428ee220b05a19aaf65b16030
Viet Vu <jooservices@gmail.com>
2022-06-16 15:08:09 +0700

    Update multi-php.sh

--- multi-php.sh
+++ multi-php.sh
@@ -6,18 +6,19 @@ sudo apt install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update && sudo apt upgrade -y
 
-phpVersions=('7.1', '7.2', '7.3', '7.4' '8.0' '8.1')
-phpExtensions=('dev', 'cli', 'mbstring', 'curl', 'intl', 'mbstring', 'mcrypt', 'xml', 'xmlrpc', 'xsl', 'yaml', 'zip', 'imagick', 'gd', 'opcache', 'memcache', 'memcached', 'mysql', 'sqlite3')
-phpPecls=('mongodb', 'redis', 'pcov', 'apcu')
-
+phpVersions=('7.1' '7.2' '7.3' '7.4' '8.0' '8.1')
+phpExtensions=('dev' 'cli' 'mbstring' 'curl' 'intl' 'mbstring' 'mcrypt' 'xml' 'xmlrpc' 'xsl' 'yaml' 'zip' 'imagick' 'gd' 'opcache' 'memcache' 'memcached' 'mysql' 'sqlite3')
+phpPecls=('mongodb' 'redis' 'pcov' 'apcu')
 
 for phpVersion in "${phpVersions[@]}"
 do
+  echo "Install PHP ${phpVersion} extensions"
   for phpExtension in "${phpExtensions[@]}"
   do
     sudo apt install php"${phpVersion}"-"${phpExtension}"
   done
 
+  echo "Install PHP ${phpVersion} pecl extensions"
   for phpPecl in "${phpPecls[@]}"
   do
     sudo pecl -d php_suffix="${phpVersion}" install "${phpPecl}"
