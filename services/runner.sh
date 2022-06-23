5829c3ffb47bf6f87d30d0a8d78881abfdd6aa13
Viet Vu <jooservices@gmail.com>
2022-06-23 22:59:44 +0700

    Update runner.sh

--- services/runner.sh
+++ services/runner.sh
@@ -9,3 +9,7 @@ mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-${version}.tar.gz -L https://github.com/actions/runner/releases/download/v${version}/actions-runner-linux-x64-${version}.tar.gz
 #echo "${hash}  actions-runner-linux-x64-${version}.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-${version}.tar.gz
+
+read -p 'Url: ' url
+read -p 'Token: ' token
+./config.sh --url ${url} --token {$token}
