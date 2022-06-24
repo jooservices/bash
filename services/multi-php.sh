0f80a878f63463b2853206b5e20dd09ccd43020e
Viet Vu <jooservices@gmail.com>
2022-06-24 10:19:13 +0700

    Update multi-php.sh

--- services/multi-php.sh
+++ services/multi-php.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt update && sudo apt upgrade -y
 
 phpVersions=('7.2' '7.3' '7.4' '8.0' '8.1')
-phpExtensions=('dev' 'cli' 'mbstring' 'curl' 'intl' 'mbstring' 'mcrypt' 'xml' 'xmlrpc' 'xsl' 'yaml' 'zip' 'imagick' 'gd' 'opcache' 'memcache' 'memcached' 'mysql' 'sqlite3')
+phpExtensions=('dev' 'cli' 'mbstring' 'curl' 'intl' 'mbstring' 'mcrypt' 'xml' 'xmlrpc' 'xsl' 'yaml' 'zip' 'imagick' 'gd' 'opcache' 'memcache' 'memcached' 'mysql' 'sqlite3' 'ldap')
 phpPecls=('mongodb' 'redis' 'pcov' 'apcu')
 
 for phpVersion in "${phpVersions[@]}"
