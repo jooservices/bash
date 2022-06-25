1ea84de8c52ab5724924c1dadf9d315872333f88
Viet Vu <jooservices@gmail.com>
2022-06-25 10:40:20 +0700

    Update git.sh

--- services/git.sh
+++ services/git.sh
@@ -8,4 +8,4 @@ sudo apt -y install git
 git config --global user.email "${email}"
 git config --global user.name "${name}"
 
-yes '' | ssh-keygen -t ed25519 -C $email
+ssh-keygen -q -t ed25519 -N '' -f ~/.ssh/id_ed25519 <<<y >/dev/null 2>&1
