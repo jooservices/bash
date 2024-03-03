1da55158491c64c922e355f0e3e53572e3e7ce3f
Viet Vu <jooservices@gmail.com>
2024-03-03 19:58:04 +0700

    Update docker.sh

--- services/docker.sh
+++ services/docker.sh
@@ -14,7 +14,7 @@ echo \
 sudo apt-get update
 
 echo 'Install'
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
