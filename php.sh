a053e780152ce4ec14b78c9d2e40a9158ac2ca83
Viet Vu <jooservices@gmail.com>
2024-11-05 12:33:21 +0700

    Update php.sh

--- php.sh
+++ php.sh
@@ -6,11 +6,7 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update && sudo apt upgrade -y
 
-# Removed : 7.4
-# Remove : 8.0
-# Remove : 8.1
-# Remove : 8.2
-phpVersions=('8.3')
+phpVersions=('7.4' '8.0' '8.1' '8.3')
 phpExtensions=('dev' 'cli' 'mbstring' 'curl' 'intl' 'mbstring' 'xml' 'xmlrpc' 'xsl' 'yaml' 'zip' 'imagick' 'gd' 'opcache' 'memcache' 'memcached' 'mysql' 'sqlite3' 'ldap' 'bcmath' 'fpm')
 phpPecls=('mongodb' 'redis' 'pcov' 'apcu')
 
@@ -20,12 +16,13 @@ do
   extensions=$(printf "php${phpVersion}-%s " "${phpExtensions[@]}")
   sudo apt install -y $extensions
 
-  echo "Install PHP ${phpVersion} pecl extensions"
-  for phpPecl in "${phpPecls[@]}"
-  do
-    sudo pecl -d php_suffix="${phpVersion}" install "${phpPecl}"
-    sudo pecl uninstall -r "${phpPecl}"    
-    sudo bash -c 'echo "extension='${phpPecl}'.so" >> /etc/php/'${phpVersion}'/cli/php.ini'
-    sudo bash -c 'echo "extension='${phpPecl}'.so" >> /etc/php/'${phpVersion}'/fpm/php.ini'
-  done
+  #echo "Install PHP ${phpVersion} pecl extensions"
+  # There are issues while installing via Pecl
+  #for phpPecl in "${phpPecls[@]}"
+  #do
+  #  sudo pecl -d php_suffix="${phpVersion}" install "${phpPecl}"
+  #  sudo pecl uninstall -r "${phpPecl}"    
+  #  sudo bash -c 'echo "extension='${phpPecl}'.so" >> /etc/php/'${phpVersion}'/cli/php.ini'
+  #  sudo bash -c 'echo "extension='${phpPecl}'.so" >> /etc/php/'${phpVersion}'/fpm/php.ini'
+  #done
 done
