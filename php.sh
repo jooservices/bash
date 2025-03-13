27f371015eb6df8d3b24ba40a66f0b0edc2ebc0a
Viet Vu <jooservices@gmail.com>
2025-03-13 08:27:48 +0700

    Update php.sh

--- php.sh
+++ php.sh
@@ -6,7 +6,7 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update && sudo apt upgrade -y
 
-phpVersions=('7.4' '8.0' '8.1' '8.3')
+phpVersions=('8.0' '8.1' '8.3')
 phpExtensions=('dev' 'cli' 'mbstring' 'curl' 'intl' 'mbstring' 'xml' 'xmlrpc' 'xsl' 'yaml' 'zip' 'imagick' 'gd' 'opcache' 'memcache' 'memcached' 'mysql' 'sqlite3' 'ldap' 'bcmath' 'fpm')
 phpPecls=('mongodb' 'redis' 'pcov' 'apcu')
 
@@ -16,13 +16,32 @@ do
   extensions=$(printf "php${phpVersion}-%s " "${phpExtensions[@]}")
   sudo apt install -y $extensions
 
-  #echo "Install PHP ${phpVersion} pecl extensions"
-  # There are issues while installing via Pecl
-  #for phpPecl in "${phpPecls[@]}"
-  #do
-  #  sudo pecl -d php_suffix="${phpVersion}" install "${phpPecl}"
-  #  sudo pecl uninstall -r "${phpPecl}"    
-  #  sudo bash -c 'echo "extension='${phpPecl}'.so" >> /etc/php/'${phpVersion}'/cli/php.ini'
-  #  sudo bash -c 'echo "extension='${phpPecl}'.so" >> /etc/php/'${phpVersion}'/fpm/php.ini'
-  #done
+  echo "Install PHP ${phpVersion} pecl extensions"
+  for phpPecl in "${phpPecls[@]}"
+  do
+    sudo pecl -d php_suffix="${phpVersion}" install "${phpPecl}"
+    sudo pecl uninstall -r "${phpPecl}"    
+    sudo bash -c 'echo "extension='${phpPecl}'.so" >> /etc/php/'${phpVersion}'/cli/php.ini'
+    sudo bash -c 'echo "extension='${phpPecl}'.so" >> /etc/php/'${phpVersion}'/fpm/php.ini'
+  done
+done
+
+phpVersions=('7.4')
+phpExtensions=('dev' 'cli' 'mbstring' 'curl' 'intl' 'mbstring' 'xml' 'xmlrpc' 'xsl' 'yaml' 'zip' 'imagick' 'gd' 'opcache' 'memcache' 'memcached' 'mysql' 'sqlite3' 'ldap' 'bcmath' 'fpm' 'mongodb' 'redis')
+
+for phpVersion in "${phpVersions[@]}"
+do
+  echo "Install PHP ${phpVersion} extensions"
+  extensions=$(printf "php${phpVersion}-%s " "${phpExtensions[@]}")
+  sudo apt install -y $extensions
+done
+
+phpVersions=('5.6')
+phpExtensions=('dev' 'cli' 'mbstring' 'curl' 'intl' 'mbstring' 'mcrypt' 'xml' 'xmlrpc' 'xsl' 'yaml' 'zip' 'imagick' 'gd' 'opcache' 'memcache' 'memcached' 'mysql' 'sqlite3' 'ldap' 'bcmath' 'fpm' 'mongodb' 'redis')
+
+for phpVersion in "${phpVersions[@]}"
+do
+  echo "Install PHP ${phpVersion} extensions"
+  extensions=$(printf "php${phpVersion}-%s " "${phpExtensions[@]}")
+  sudo apt install -y $extensions
 done
